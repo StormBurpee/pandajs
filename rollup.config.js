e7c4dbded5321d36893e07f32b08d299c09fb9bd
Storm Burpee <stormburpee@gmail.com>
2020-02-12 19:48:19 +1030

    Added base configuration for building

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import path from 'path'
+import typescript from 'rollup-plugin-typescript2';
+
+export default [
+    {
+        input: './src/compiler/entry.ts',
+        output: [
+            {
+                file: 'dist/compiler/compiler.js',
+                format: 'cjs'
+            },
+            {
+                file: 'dist/compiler/compiler.mjs',
+                format: 'esm'
+            }
+        ],
+        plugins: [typescript({
+            include: 'src/**',
+            typescript: require('typescript'),
+            tsconfig: path.resolve(__dirname, 'src', 'compiler', 'tsconfig.json')
+        })]
+    }
+];
